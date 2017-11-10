43922803cf1ec1bb30cd5125408ae35b5b134dba
Alexander Walters <tritium@sdamon.com>
2017-11-10 00:20:38 -0500

    Update iteration.py

--- Python/iteration.py
+++ Python/iteration.py
@@ -1,4 +1,7 @@
 def group(iterable, size):
     "Group items in iterable in sub-iterables of size"
-    from itertools import zip_longest
+    try:
+        from itertools import zip_longest
+    except ImportError:
+        from itertools import izip_longest as zip_longest
     return (tuple(filter(None, x)) for x in zip_longest(*([iter(iterable)]*size)))
