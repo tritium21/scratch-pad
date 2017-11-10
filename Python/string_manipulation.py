aaeebb6c739c070760cf386c29e95f597349f66b
Alexander Walters <tritium@sdamon.com>
2017-11-10 00:25:33 -0500

    Update string_manipulation.py

--- Python/string_manipulation.py
+++ Python/string_manipulation.py
@@ -1,7 +1,15 @@
 def rot13(s):
     "ROT-13 Ceaser Cypher"
-    return s.translate(
-        str.maketrans(
+    try:
+        import string
+        import functools
+        maketrans = string.maketrans
+        translate = functools.partial(string.translate, s)
+    except AttributeError:
+        maketrans = str.maketrans
+        translate = s.translate
+    return translate(
+        maketrans(
             "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz",
             "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm"
          )
