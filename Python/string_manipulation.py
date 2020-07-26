f278cbdc32a2f226f55b8ecbd5ce5277565b1fe3
Alexander Walters <1781587+tritium21@users.noreply.github.com>
2020-07-26 17:20:12 -0400

    Drop python 2 support

--- Python/string_manipulation.py
+++ Python/string_manipulation.py
@@ -1,15 +1,7 @@
 def rot13(s):
     "ROT-13 Ceaser Cypher"
-    try:
-        import string
-        import functools
-        maketrans = string.maketrans
-        translate = functools.partial(string.translate, s)
-    except AttributeError:
-        maketrans = str.maketrans
-        translate = s.translate
-    return translate(
-        maketrans(
+    return s.translate(
+        str.maketrans(
             "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz",
             "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm"
          )
