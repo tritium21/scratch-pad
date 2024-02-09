99759167f959fbfd362e2b925c17ec80fa61a763
tritium21 <1781587+tritium21@users.noreply.github.com>
2024-02-09 17:41:23 -0500

    Update atomic_open.py

--- Python/atomic_open.py
+++ Python/atomic_open.py
@@ -1,19 +1,9 @@
 import contextlib
-import ctypes
 import os
 import pathlib
 import shutil
 import sys
 import tempfile
-
-win = sys.platform == 'win32'
-
-
-def replace_file(old, new)
-    if win:
-        ctypes.windll.kernel32.ReplaceFileW(old, new, None, 0x2, None, None)
-        return
-    os.rename(new, old)
 
 
 @contextlib.contextmanager
@@ -21,9 +11,14 @@ def atomic(path, mode='r', buffering=-1, encoding=None, errors=None, newline=Non
     name = path = pathlib.Path(path)
     exists = path.is_file()
     if exists:
-        name = tempfile.mktemp(prefix=path.name(path), dir=path.parent)
+        name = pathlib.Path(tempfile.mktemp(prefix=path.name(path), dir=path.parent))
         shutil.copy2(path, name)
     with open(name, mode=mode, buffering=buffering, encoding=encoding, errors=errors, newline=newline) as tf:
-        yield tf
+        try:
+            yield tf
+        except Cancel:
+            if not exists:
+                pathlib.Path(name).unlink()
+            return
     if exists:
-        replace_file(path, name)
+        name.replace(path)
